a1ea54ba643f1a6db6a557e41f85fe9752060aa3
Alex Barker <alexbarker31@gmail.com>
2023-08-18 22:09:33 +0100

    18/08/2023

--- GoldLoot.py
+++ GoldLoot.py
@@ -1,2 +1,23 @@
-players = input("Number of players: ")
-baseGP = input("Base gold each: ")
+import math
+import time
+
+players = int(input("Number of players: "))
+baseGP = int(input("Base gold each: "))
+maxBonus = int(input("Maximum extra each: "))
+if maxBonus < 4:
+    dice = 2
+elif maxBonus < 6:
+    dice = 4
+elif maxBonus < 8:
+    dice = 6
+elif maxBonus < 10:
+    dice = 8
+elif maxBonus < 12:
+    dice = 10
+elif maxBonus < 100:
+    dice = 20
+else:
+    dice = 100
+print("/r ",math.floor((maxBonus/dice))*players,"d",dice,"+",baseGP*players,sep='')
+
+time.sleep(10)
